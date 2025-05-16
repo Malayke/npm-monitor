4502059221e6eadd0cd0f987fb5d40a18b77dafa
Merdan Aziz <merdan.aziz@coins.ph>
2025-05-17 00:32:59 +0800

    change table schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS npm_packages (
-    name TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
     version TEXT NOT NULL,
     published_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
